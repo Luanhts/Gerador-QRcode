9b8bfd82501d2da821a8db3ecfa1598911b02ecd
Luan <132374218+Luanhts@users.noreply.github.com>
2024-12-23 20:46:39 -0300

    feat: estilização

--- qrgenerator/src/App.jsx
+++ qrgenerator/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Gerador de Qr Code</h1>
+        <div className="img-box">
+          <img src="https://github.com/Luanhts.png" alt="" />
+        </div>
+        <input type="url" placeholder="Digite a URL" />
+        <button type="submit">Gerar Qr Code</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
